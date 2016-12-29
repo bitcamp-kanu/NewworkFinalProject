dfb816404bb350e2523aee6af6867d191773b46c
DESKTOP-NKG80J0\gram <found75@naver.com>
2016-12-29 10:12:53 +0900

    WIDisplay 함수 추가. (화면 그릴때 사용하는 클래서스)

---
+++ NewworkFinalProject/Public/WIDisplay.cpp
@@ -0,0 +1,104 @@
+#pragma comment(lib,"Ws2_32.lib") //winsock 2.2 라이브러리
+#define WINVER 0x0501
+#define _WIN32_WINNT 0x0501
+
+#include <WinSock2.h> //Winsock 2 버전 Header
+#include "WIDisplay.h"
+#include "WIUtility.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char Display::m_cKey = '*';
+
+//가로 라인은 그린다.
+void Display::DrawXLine(Point& pt1 , int len)
+{
+	for(int i = 0 ; i <len ; i++ )
+	{	
+		WIUtility::Gotoxy(pt1.GetX() + i , pt1.GetY());
+		printf("%c",Display::m_cKey);
+	}
+}
+//세로 라인을 그린다.
+void Display::DrawYLine(Point& pt1 , int len)
+{
+	for(int i = 0 ; i <len ; i++ )
+	{		
+		WIUtility::Gotoxy(pt1.GetX() , pt1.GetY() + i);
+		printf("%c",Display::m_cKey);
+	}
+}
+
+void Display::DrawRect(Point& pt ,Point& pt2)
+{
+	for(int i = 0 ; i <pt2.GetX() ; i++ )
+	{	
+		int j=pt2.GetY();
+		WIUtility::Gotoxy(pt.GetX() + i , pt.GetY());
+		printf("%c",Display::m_cKey);
+		WIUtility::Gotoxy(pt.GetX() + i+1 , pt.GetY()+j);
+		printf("%c",Display::m_cKey);
+	}
+	for(int i = 0 ; i < pt2.GetY() ; i++ )
+	{	
+		int j=pt2.GetX();	
+		WIUtility::Gotoxy(pt.GetX()  , pt.GetY()+i);
+		printf("%c",Display::m_cKey);
+		WIUtility::Gotoxy(pt.GetX() + j, pt.GetY()+i+1);
+		printf("%c",Display::m_cKey);
+	}
+}
+
+//2016.12.13 메소드 추가.
+void Display::DrawRect(Rect& rect)
+{
+	Display::DrawXLine(rect.GetStaPos(),rect.GetWidth());
+	Point newPt = rect.GetStaPos() + Point(0,rect.GetHeigth()-1);
+	Display::DrawXLine(newPt,rect.GetWidth());
+
+	Display::DrawYLine(rect.GetStaPos(),rect.GetHeigth());
+	newPt = rect.GetStaPos() + Point(rect.GetWidth()-1,0);
+	Display::DrawYLine(newPt,rect.GetHeigth());
+}
+char Display::IsSaveMenu(int x,int y)
+{
+	//char buff[255] = {0};
+	int key = 0;
+	while(true)
+	{
+		WIUtility::Gotoxy(x,y);
+		cout << "                                                         ";
+		WIUtility::Gotoxy(x,y);
+		cout << "저장 하시겠습니까. 저장(Y) 취소(C) 다시입력(N)";		
+		key = getch();
+		if(toupper(key) == 'Y' || toupper(key) == 'C' || toupper(key) == 'N')
+		{
+			WIUtility::Gotoxy(x,y);
+			cout << "                                                              "; //지정된 데이터를 삭제 하나,
+			break;
+		}
+	}
+	return toupper(key);
+}
+char Display::IsSelectMenu(int x,int y)
+{
+	int key = 0;
+	while(true)
+	{		
+		WIUtility::Gotoxy(x,y);
+		key = getch();
+		if(toupper(key) == 'Y' || toupper(key) == 'C' || toupper(key) == 'N')
+		{
+			WIUtility::Gotoxy(x,y);
+			cout << "                                                              "; //지정된 데이터를 삭제 하나,
+			break;
+		}
+	}
+	return toupper(key);
+}
+
---
+++ NewworkFinalProject/Public/WIDisplay.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <windows.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Point
+{
+private:
+	int m_nX;
+	int m_nY;
+
+public:
+	Point()
+	{
+		m_nX = m_nY = 0;
+	}
+	Point(const int& x,const int& y)
+	{
+		m_nX = x;
+		m_nY = y;
+
+	}
+	int GetX()
+	{
+		return m_nX;
+	}
+	void SetX(const int x)
+	{
+		m_nX = x;
+	}
+	int AddX(int x)
+	{
+		m_nX += x;
+	}
+
+	int GetY()
+	{
+		return m_nY;
+	}
+	void SetY(const int y)
+	{
+		m_nY = y;
+	}
+	int AddY(int y)
+	{
+		m_nY += y;
+	}
+
+	Point& operator+(Point& pt)
+	{
+		m_nX  = m_nX + pt.GetX();
+		m_nY += pt.GetY();
+		return (*this);
+	}
+
+};
+class Rect
+{
+private:
+	int m_nX;
+	int m_nY;
+	int m_nWidth;
+	int m_nHeight;
+public:
+	Rect(int x,int y, int width, int height)
+	{
+		m_nX = x;
+		m_nY = y;
+		m_nWidth = width;
+		m_nHeight = height;
+	}
+
+	int GetX()
+	{
+		return m_nX;
+	}
+	int GetY()
+	{
+		return m_nY;
+	}
+	int GetWidth()
+	{
+		return m_nWidth;
+	}
+	int GetHeigth()
+	{
+		return m_nHeight;
+	}
+	Point GetStaPos()
+	{
+		return Point(m_nX,m_nY);
+	}
+
+	Rect InnerRect() //사방으로 1직 작은 사각형 반환
+	{
+		return Rect(m_nX + 1,m_nY + 1,m_nWidth - 2,m_nWidth - 2);
+	}
+};
+
+
+class Display
+{
+private:
+	static char m_cKey;
+public:
+	void static DrawRect(Rect& rect);
+	
+	void static DrawRect(Point& pt , Point& pt2);
+	void static DrawXLine(Point& pt1 , int len);	
+	void static DrawYLine(Point& pt1 , int len);
+
+	char static IsSaveMenu(int x,int y);
+	char static IsSelectMenu(int x,int y);
+};
