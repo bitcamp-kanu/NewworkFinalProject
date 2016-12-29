f5ef890529abd0fe444de189795236cabc08f008
DESKTOP-NKG80J0\gram <found75@naver.com>
2016-12-30 01:16:14 +0900

    [add] class DBManager 추가. DB 접속 및 조회 저장 삭제를 관리 하는 클래스.

---
+++ NewworkFinalProject/DBSevr/DBManager.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <stdio.h>
+////#include <windows.h>
+//#include <afxwin.h> 
+//#include <afxdb.h> //ODBC
+//#include <cstring>
+#include <string>
+#include <vector>
+using namespace std;
+
+class CDatabase;
+class DBManager
+{
+private:
+	CDatabase* m_podb;	
+public:
+
+	bool Open();
+	//로그인을 검사 한다.
+	bool IsUserPassword(string id, string pass);
+	vector<string> Select(string& query);
+	DBManager(void);
+	//DBManager(CString& str);
+	virtual ~DBManager(void);
+};
+
