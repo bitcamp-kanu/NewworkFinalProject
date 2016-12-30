ddbdfc008fad8582db475123c248f6f444bcfa58
juny37 <juny37@gmail.com>
2016-12-30 10:41:47 +0900

    영준 작업 내역

--- NewworkFinalProject/GateWaySevr/Config.cpp
+++ NewworkFinalProject/GateWaySevr/Config.cpp
@@ -9,18 +9,29 @@ Config::Config(void)
 
 Config::~Config(void)
 {
-	
+
 }
 
 
 bool Config::SaveConfig()
 {
+
+	m_dbServerIP = "127.0.0.1";
+	m_nDbServerPort = 9001;
+	return true;
+
 	//WIFile m_pKnFile;
 	//m_pKnFile.Open(m_strFileName,"a+");
 	//m_pKnFile.WriteLine(string(buff));
-	return true;
 }
 bool Config::LoadConfig()
 {
+	m_nServerPort = 9000;
+	m_nListenCnt = 5;
+
+	//연결할 DB 서버 정보. 
+	m_dbServerIP = "127.0.0.1";
+	m_nDbServerPort = 9001;
+
 	return true;
 }
--- NewworkFinalProject/Public/PublicDefine.h
+++ NewworkFinalProject/Public/PublicDefine.h
@@ -8,14 +8,20 @@ using namespace std;
 #define _RECV_BUFFER_SIZE 2048
 #define _ID_SIZE_ 20
 #define _PASS_SIZE_ 20
+
+//[LYJ 시작]
+#define _CLASSID_SIZE_ 20
+#define _NAME_SIZE_ 20
+#define _DATE_SIZE_ 16
+//[LYJ 끝]
 //사용할 패킷을 정의 한다.
 
 struct _Header
-{	
+{
 	char cmd1;
 	char cmd2;
 	int  pakID; //Ack/Nak 확인 용으로 사용
-		//--- 인증 관련 으로 무조건 필요.
+				//--- 인증 관련 으로 무조건 필요.
 	char id[_ID_SIZE_];	    //자릿 수는 추후 정해야 함.
 	char SecretKey; //인증키.
 };
@@ -28,40 +34,78 @@ struct _Login
 	_Header header;
 	char id[_ID_SIZE_];
 	char pass[_PASS_SIZE_];
-	_Login(){};
+	_Login() {};
 	char SecretKey; //인증키.
 	int cont;
-	_Login(char cmd1,char cmd2,int  pakID,char* id,char* pass)
+	_Login(char cmd1, char cmd2, int  pakID, char* id, char* pass)
 	{
-		header.cmd1		= cmd1;
-		header.cmd2		= cmd2;
-		header.pakID	= pakID;
-		strcpy(header.id,id);
-		strcpy(this->id,id);
-		strcpy(this->pass,pass);
+		header.cmd1 = cmd1;
+		header.cmd2 = cmd2;
+		header.pakID = pakID;
+		strcpy(header.id, id);
+		strcpy(this->id, id);
+		strcpy(this->pass, pass);
 		cont = 0;
 	}
 
 	//내자신을 초기화 한다.
 	void InitData()
 	{
-		memset(this,0x00,sizeof(this));
+		memset(this, 0x00, sizeof(this));
 	}
 	//buff 의 내용을 구조체에 채운다.
 	void SetCopyBuff(char* data)
 	{
-		memcpy(this,data,sizeof(this));
+		memcpy(this, data, sizeof(this));
 	}
 	string ToString()
 	{
 		char buff[1024];
-		sprintf(buff,"%c%c key[%c] , id [%d] 순서[%d]",header.cmd1,header.cmd2,header.pakID,id,cont);
+		sprintf(buff, "%c%c key[%c] , id [%d] 순서[%d]", header.cmd1, header.cmd2, header.pakID, id, cont);
+		return string(buff);
+	}
+};
+
+//Gate AL 이영준
+struct _Gate
+{
+	_Header header;
+	char id[_ID_SIZE_];
+	char pass[_PASS_SIZE_];
+	_Gate() {};
+	char SecretKey; //인증키.
+	int cont;
+	_Gate(char cmd1, char cmd2, int  pakID, char* id, char* pass)
+	{
+		header.cmd1 = cmd1;
+		header.cmd2 = cmd2;
+		header.pakID = pakID;
+		strcpy(header.id, id);
+		strcpy(this->id, id);
+		strcpy(this->pass, pass);
+		cont = 0;
+	}
+
+	//내자신을 초기화 한다.
+	void InitData()
+	{
+		memset(this, 0x00, sizeof(this));
+	}
+	//buff 의 내용을 구조체에 채운다.
+	void SetCopyBuff(char* data)
+	{
+		memcpy(this, data, sizeof(this));
+	}
+	string ToString()
+	{
+		char buff[1024];
+		sprintf(buff, "%c%c key[%c] , id [%d] 순서[%d]", header.cmd1, header.cmd2, header.pakID, id, cont);
 		return string(buff);
 	}
 };
 
 //학생검색 SR -- 서정민
-struct _SearchStudent 
+struct _SearchStudent
 {
 
 };
@@ -72,22 +116,196 @@ struct _CreateStudent
 
 };
 
-//학생 삭제. SD  - 영준
+//학생 삭제. SD  - 영준 // 추후 필요없는 컬럼 빼야함.
 struct _DeleteStudent
 {
+	_Header header;
+	int ClassNum;
+	char ClassId[_CLASSID_SIZE_];
+	int Seq;
+	char SName[_NAME_SIZE_];
+	int C_Score;
+	int Cpp_Score;
+	int Cshart_Score;
+	int Network_Score;
+	int Unity_Score;
+	int Total;
+	float Avg;
+	char UDate[_DATE_SIZE_];
+	char Flag;
+	_DeleteStudent() {};
+	int cont;
+	_DeleteStudent(char cmd1, char cmd2, int  pakID, char* id,
+		int ClassNum, char* ClassId, int Seq, char* SName, int C_Score, int Cpp_Score,
+		int Cshart_Score, int Network_Score, int Unity_Score, int Total, float Avg,
+		char* UDate, char Flag)
+	{
+		header.cmd1 = cmd1;
+		header.cmd2 = cmd2;
+		header.pakID = pakID;
+		strcpy(header.id, id);
+		this->ClassNum = ClassNum;
+		strcpy(this->ClassId, ClassId);
+		this->Seq = Seq;
+		strcpy(this->SName, SName);
+		this->C_Score = C_Score;
+		this->Cpp_Score = Cpp_Score;
+		this->Cshart_Score = Cshart_Score;
+		this->Network_Score = Network_Score;
+		this->Unity_Score = Unity_Score;
+		this->Total = Total;
+		this->Avg = Avg;
+		strcpy(this->UDate, UDate);
+		this->Flag = Flag;
+		cont = 0;
+	}
 
+	//내자신을 초기화 한다.
+	void InitData()
+	{
+		memset(this, 0x00, sizeof(this));
+	}
+	//buff 의 내용을 구조체에 채운다.
+	void SetCopyBuff(char* data)
+	{
+		memcpy(this, data, sizeof(this));
+	}
+	string ToString()
+	{
+		char buff[1024];
+		sprintf(buff, "%c%c key[%c] , id [%d] 순서[%d]", header.cmd1, header.cmd2, header.pakID,
+			ClassNum, ClassId, Seq, SName, C_Score, Cpp_Score, Cshart_Score, Network_Score,
+			Unity_Score, Total, Avg, UDate, Flag);
+		return string(buff);
+	}
 };
 
-//학생 수정. SU   - 영준
+//학생 수정. SU   - 영준 // 추후 필요없는 컬럼 빼야함.
 struct _UpdateStudent
 {
+	_Header header;
+	int ClassNum;
+	char ClassId[_CLASSID_SIZE_];
+	int Seq;
+	char SName[_NAME_SIZE_];
+	int C_Score;
+	int Cpp_Score;
+	int Cshart_Score;
+	int Network_Score;
+	int Unity_Score;
+	int Total;
+	float Avg;
+	char UDate[_DATE_SIZE_];
+	char Flag;
+	_UpdateStudent() {};
+	int cont;
+	_UpdateStudent(char cmd1, char cmd2, int  pakID, char* id,
+		int ClassNum, char* ClassId, int Seq, char* SName, int C_Score, int Cpp_Score,
+		int Cshart_Score, int Network_Score, int Unity_Score, int Total, float Avg,
+		char* UDate, char Flag)
+	{
+		header.cmd1 = cmd1;
+		header.cmd2 = cmd2;
+		header.pakID = pakID;
+		strcpy(header.id, id);
+		this->ClassNum = ClassNum;
+		strcpy(this->ClassId, ClassId);
+		this->Seq = Seq;
+		strcpy(this->SName, SName);
+		this->C_Score = C_Score;
+		this->Cpp_Score = Cpp_Score;
+		this->Cshart_Score = Cshart_Score;
+		this->Network_Score = Network_Score;
+		this->Unity_Score = Unity_Score;
+		this->Total = Total;
+		this->Avg = Avg;
+		strcpy(this->UDate, UDate);
+		this->Flag = Flag;
+		cont = 0;
+	}
 
+	//내자신을 초기화 한다.
+	void InitData()
+	{
+		memset(this, 0x00, sizeof(this));
+	}
+	//buff 의 내용을 구조체에 채운다.
+	void SetCopyBuff(char* data)
+	{
+		memcpy(this, data, sizeof(this));
+	}
+	string ToString()
+	{
+		char buff[1024];
+		sprintf(buff, "%c%c key[%c] , id [%d] 순서[%d]", header.cmd1, header.cmd2, header.pakID,
+			ClassNum, ClassId, Seq, SName, C_Score, Cpp_Score, Cshart_Score, Network_Score,
+			Unity_Score, Total, Avg, UDate, Flag);
+		return string(buff);
+	}
 };
 
-//성적 수정. GU  - 영준
+//성적 수정. GU  - 영준 // 추후 필요없는 컬럼 빼야함.
 struct _UpdateGrade
 {
+	_Header header;
+	int ClassNum;
+	char ClassId[_CLASSID_SIZE_];
+	int Seq;
+	char SName[_NAME_SIZE_];
+	int C_Score;
+	int Cpp_Score;
+	int Cshart_Score;
+	int Network_Score;
+	int Unity_Score;
+	int Total;
+	float Avg;
+	char UDate[_DATE_SIZE_];
+	char Flag;
+	_UpdateGrade() {};
+	int cont;
+	_UpdateGrade(char cmd1, char cmd2, int  pakID, char* id,
+		int ClassNum, char* ClassId, int Seq, char* SName, int C_Score, int Cpp_Score,
+		int Cshart_Score, int Network_Score, int Unity_Score, int Total, float Avg,
+		char* UDate, char Flag)
+	{
+		header.cmd1 = cmd1;
+		header.cmd2 = cmd2;
+		header.pakID = pakID;
+		strcpy(header.id, id);
+		this->ClassNum = ClassNum;
+		strcpy(this->ClassId, ClassId);
+		this->Seq = Seq;
+		strcpy(this->SName, SName);
+		this->C_Score = C_Score;
+		this->Cpp_Score = Cpp_Score;
+		this->Cshart_Score = Cshart_Score;
+		this->Network_Score = Network_Score;
+		this->Unity_Score = Unity_Score;
+		this->Total = Total;
+		this->Avg = Avg;
+		strcpy(this->UDate, UDate);
+		this->Flag = Flag;
+		cont = 0;
+	}
 
+	//내자신을 초기화 한다.
+	void InitData()
+	{
+		memset(this, 0x00, sizeof(this));
+	}
+	//buff 의 내용을 구조체에 채운다.
+	void SetCopyBuff(char* data)
+	{
+		memcpy(this, data, sizeof(this));
+	}
+	string ToString()
+	{
+		char buff[1024];
+		sprintf(buff, "%c%c key[%c] , id [%d] 순서[%d]", header.cmd1, header.cmd2, header.pakID,
+			ClassNum, ClassId, Seq, SName, C_Score, Cpp_Score, Cshart_Score, Network_Score,
+			Unity_Score, Total, Avg, UDate, Flag);
+		return string(buff);
+	}
 };
 
 //과목별 평균. AA -- 승욱 형님.
