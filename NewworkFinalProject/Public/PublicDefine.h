554c426029e4f720342208df4bf27a4855da3456
DESKTOP-NKG80J0\gram <found75@naver.com>
2016-12-30 16:41:05 +0900

    경고관련 코드 추가.

--- NewworkFinalProject/Public/PublicDefine.h
+++ NewworkFinalProject/Public/PublicDefine.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#pragma warning(disable: 4996)
+#pragma warning(disable: 4081)
 #include <iostream>
 #include <string>
 using namespace std;
@@ -10,19 +12,30 @@ using namespace std;
 #define _PASS_SIZE_ 20
 //사용할 패킷을 정의 한다.
 
+//pakID
+//100 로그인
+//	110 로그인 실패.
+//	111 로성공
+//200 인증관련
+// 201 인증 성공
+// 200 인증 실패 - 보인키가 없음.
+// 210 인증 실패 - 보안키 유효 시간이 초과 되었음. 
 
+
+//A 전체, S 학생 G 성적
+#pragma pack(push,1)
 struct _Header
 {
 	char cmd1;
 	char cmd2;
-	int  pakID; //Ack/Nak 확인 용으로 사용
+	int  pakID; //111 성공 110 실패  //인증실패 211
 		//--- 인증 관련 으로 무조건 필요.
 	char id[_ID_SIZE_];	    //자릿 수는 추후 정해야 함.
 	char SecretKey; //인증키.
 };
-
-//A 전체, S 학생 G 성적
-#pragma pack(push 1)
+//prefix All:전체 Student::학생 Grade:성적  Etc:기타 
+// 
+//
 
 //Login AL 서정민
 struct _Login
@@ -63,6 +76,40 @@ struct _Login
 	}
 };
 
+//보안키 인증 - 보안키를 확인 한다. EC
+struct _SecretKeyChedk
+{
+	_Header header;
+	char	chBSucess; //'T'  성공
+
+	_SecretKeyChedk(char cmd1, char cmd2, int  pakID, char* id, char chBSucess)
+	{
+		header.cmd1 = cmd1;
+		header.cmd2 = cmd2;
+		header.pakID = pakID;
+		strcpy(header.id, id);
+		
+		chBSucess = chBSucess;
+	}
+	void InitData()
+	{
+		memset(this, 0x00, sizeof(this));
+	}
+
+	//buff 의 내용을 구조체에 채운다.
+	void SetCopyBuff(char* data)
+	{
+		memcpy(this, data, sizeof(this));
+	}
+
+	string ToString()
+	{
+		char buff[1024];
+		sprintf(buff,"%c%c pakID[%d] , id [%s], chBSucess [%c] ",header.cmd1,header.cmd2,header.pakID,header.id,chBSucess);
+		return string(buff);
+	}
+};
+
 
 
 struct _SearchStudent
@@ -98,5 +145,4 @@ struct _AverageAll
 {
 
 };
-
 #pragma pack(pop)
--- NewworkFinalProject/Public/WIFile.cpp
+++ NewworkFinalProject/Public/WIFile.cpp
@@ -1,3 +1,4 @@
+#pragma warning(disable: 4996)
 #include "WIFile.h"
 #include <iostream>
 #include <string>
